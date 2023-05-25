d39bcf849e2416edf88757db0ed0a52796c4f6ec
Josh_Min <joshuamin2006@hotmail.com>
2023-05-25 12:51:53 -0400

    f

---
+++ tests/test.py
@@ -0,0 +1,11 @@
+import pygame
+
+# Create an app object to store global variables
+class App:
+    pass
+app = App()
+
+# Set up the window
+pygame.init()
+pygame.display.set_caption('NAME OF THE GAME')
+window = pygame.display.set_mode([400, 400])
