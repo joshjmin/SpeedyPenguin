94278465d8045ca42a7aadb8598608dca5f061cf
Colin <colin.falconer03@gmail.com>
2023-05-29 10:03:46 -0400

    .

--- tests/death_test.py
+++ tests/death_test.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 window = pygame.display.set_mode((400, 400))
@@ -12,9 +13,27 @@ app.x = 50
 app.y = 350
 app.projx = 450
 app.projy = 350
+app.prox = 450
+app.proy = 350
+app.px = 450
+app.py = 350
 time = 0
 run = True
 safe = True
+com = 1
+
+#definitions
+def flyer():
+    app.prox -= 2
+    if app.prox < 100:
+        app.proy -= 2
+        app.prox -= 2
+
+def standerd():
+        app.projx -= 2
+
+def fast():
+    app.px -= 5
 
 # #from sawczak_demo import image 
 
@@ -36,16 +55,43 @@ while run:
             run = False 
 
     keys = pygame.key.get_pressed()    
-    app.projx -= 2
-
-    if app.projx < 0:
+    #make the projectile
+    if com == 1:
+        a = random.randint(0,2)
+    #reset the projectiles
+    if app.projx < -20:
         app.projx = 450
+        app.proy = 350
+        com = 1
+    if app.prox < -20:
+        app.prox = 450
+        com = 1
+    if app.px < -20:
+        app.px = 450
+        com = 1
     
+    #making the projictle moove
+    if com == 1:
+        com = 0
+        a = random.randint(0,2)
+    if a == 0:
+        app.projx -= 2
+    elif a == 1:
+        app.px -= 5
+    else:
+        #flyer
+        app.prox -= 2
+        if app.prox < 150:
+            app.proy -= 3
+            app.prox -= 2
+
+    #calculate air time to incrase the fall speed
     if app.y < 350:
         air += 0.07
     if app.y == 350:
         air = 0
     
+    #track the inputs
     if keys[pygame.K_SPACE]:
         if app.y < 50:
             pass
@@ -65,12 +111,18 @@ while run:
             time -= 1
     if app.x - app.projx < 25 and app.x - app.projx > -25 and app.y - app.projy < 25 and app.y - app.projy > -25:
         break
+    if app.x - app.prox < 35 and app.x - app.prox > -35 and app.y - app.proy < 35 and app.y - app.proy > -35:
+        break
+    if app.x - app.px < 25 and app.x - app.px > -25 and app.y - app.py < 25 and app.y - app.py > -25:
+        break
  
+    #drawings
     window.fill((0, 0, 64))
     window.blit(penguin, (app.x - 20  , app.y - 75))
     pygame.draw.circle(window, 'red' , (app.projx , app.projy) , 20)
+    pygame.draw.circle(window, 'green' , (app.px , app.py) , 20)
+    pygame.draw.circle(window, 'purple' , (app.prox , app.proy) , 20)
     pygame.draw.rect(window, 'gray', (0, 365, 400, 400))
-
 
     #jump meater
     pygame.draw.rect(window , 'gray' , (0,0, 100 , 40))
