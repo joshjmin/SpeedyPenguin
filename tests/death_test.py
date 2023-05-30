59c1fa323ed2713edac63f4822dcb8bca9df8cd9
Colin <colin.falconer03@gmail.com>
2023-05-30 11:30:41 -0400

    .

--- tests/death_test.py
+++ tests/death_test.py
@@ -1,13 +1,15 @@
+#imports
 import pygame
 import random
 
+#start window and other starting actions
 pygame.init()
 window = pygame.display.set_mode((400, 400))
 clock = pygame.time.Clock()
 class App:
     pass
 app = App()
-
+#making placements and terms
 air = 0
 app.x = 50
 app.y = 350
@@ -21,17 +23,22 @@ app.dx = 450
 app.dy = 50
 app.middlex = 450
 app.middley = 330
+app.uperx = 500
+app.upery = 500
+
 time = 0
 gtime = 0
 speed = 0 + (gtime)
+
 run = True 
-safe = True
 com = 1
 motion = 'standing'
+frame = 0
+
 #definitions
 def flyer():
     app.prox -= 4 + speed
-    if app.prox < 170:
+    if app.prox < 150:
         app.proy -= 4 + speed
         app.prox -= 2 + speed
 def standerd():
@@ -39,31 +46,40 @@ def standerd():
 def fast():
     app.px -= 6 + speed
 def diver():
-    if app.dy <= 330:
+    if app.dy <= 325:
         app.dy += 3 +speed
-    app.dx -= 1 + speed
+    app.dx -= 2 + speed
+    app.uperx = app.dx 
+    app.upery = app.dy - 30
 def middle():
     app.middlex -= 3 + speed
+    app.uperx = app.middlex 
+    app.upery = app.middley - 30
+
 #generate assets
 penguin = pygame.image.load('src/assets/penguin<3.png')    
 penguin = pygame.transform.scale(penguin , (50,50))
 ice = pygame.image.load('sawczak_demo/assets/iceberg.png')
-ice = pygame.transform.scale(ice , (50,100 ))
+ice = pygame.transform.scale(ice , (100,90 ))
 
 #main loop
 while run:
+    #ticks and frame data
     clock.tick(120)
+    #collect fram data
+    frame += 1
+    if frame == 4:
+        frame = 1
     #speed up
     gtime += 1
     speed += (gtime /  5000000)
     #what level is the speed at
     level = int((gtime // 1000) + 1)
+    
     #check if they quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False 
-    #keys means if a key is pressed
-    keys = pygame.key.get_pressed()   
 
     #reset the projectiles
     if app.projx < 0:
@@ -93,9 +109,9 @@ while run:
         elif level == 2:
             a = random.randint(0,2)
         elif level >= 3:
-            a = random.randint(0,3)
-        else:
             a = random.randint(0,4)
+        else:
+            a = random.randint(0,5)
     if a == 0:
         standerd()
     elif a == 2:
@@ -113,9 +129,10 @@ while run:
     if app.y == 350:
         air = 0
     
-
-
-    #track the inputs
+    #track inputs
+    #keys means if a key is pressed
+    keys = pygame.key.get_pressed()   
+    #what to do if preseed
     if keys[pygame.K_UP]:
         if app.y < 50:
             pass
@@ -139,9 +156,9 @@ while run:
         else:
             if app.y >= 360:
                 app.y = 360
-            app.y += 10
+            app.y += 5
             motion = 'sliding'
-    elif app.y > 350:
+    elif app.y > 360:
         app.y = 350
 
     if app.y == 350:
@@ -163,17 +180,25 @@ while run:
         break
     if app.x - app.middlex < 25 and app.x - app.middlex > -25 and app.y - app.middley < 25 and app.y - app.middley > -25:
         break
- 
+    if app.x - app.uperx < 25 and app.x - app.uperx > -25 and app.y - app.upery < 25 and app.y - app.upery > -25:
+        break
+
     #drawings
     #TODO make animation for the walking of the main player
+    #make the backround #TODO better
     window.fill((0, 0, 64))
+    #draw the charecters
     window.blit(penguin, (app.x - 30  , app.y - 30))
-    window.blit (ice, (app.projx - 30  , app.projy - 55))
+    window.blit (ice, (app.projx - 50  , app.projy - 35))
     pygame.draw.circle(window, 'green' , (app.px , app.py) , 20)
     pygame.draw.circle(window, 'purple' , (app.prox , app.proy) , 20)
-    pygame.draw.rect(window, 'gray', (0, 365, 400, 400))
+    pygame.draw.rect(window, '#A5F2F3', (0, 365, 400, 400))
     pygame.draw.circle(window , 'yellow' , (app.dx , app.dy) , 20)
     pygame.draw.circle(window , 'brown' , (app.middlex , app.middley) , 20)
+    if app.uperx == app.middlex:
+        pygame.draw.circle(window , 'brown' , (app.uperx , app.upery) , 20)
+    elif app.uperx == app.dx:
+        pygame.draw.circle(window , 'yellow' , (app.uperx , app.upery) , 20)
  
     #jump meter
     pygame.draw.rect(window , 'gray' , (0,0, 100 , 40))
@@ -201,8 +226,8 @@ while run:
         pygame.draw.rect(window, 'green' , (55 , 5 , 40 , 30))
     #making a score board
     font = pygame.font.SysFont('New times roman', 24)
-    app.text = font.render(f'LEVEL: {level}', True, 'white', '#000064')
-    window.blit(app.text , (100,375))
+    app.text = font.render(f'LEVEL: {level}', True, '#0000FF', '#A5F2F3')
+    window.blit(app.text , (300,375))
     pygame.display.flip()
 pygame.quit()
 exit()
