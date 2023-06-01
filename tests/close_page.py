ec5664232e5bcf9e4e8f5c3be966e5fd4c98df08
Julia <julia_chung@tdchristian.ca>
2023-06-01 13:11:46 -0400

    close

--- tests/close_page.py
+++ tests/close_page.py
@@ -1,40 +1,41 @@
 import pygame
 
-pygame.init()
-pygame.display.set_caption('Speedy Penguin DEATH')
-window = pygame.display.set_mode([400, 400])
+def death():
+    pygame.init()
+    pygame.display.set_caption('Speedy Penguin DEATH')
+    window = pygame.display.set_mode([400, 400])
 
-pygame.font.init()
-font = pygame.font.SysFont('New times roman', 35)
+    pygame.font.init()
+    font = pygame.font.SysFont('New times roman', 35)
 
-death = font.render('YOU DIED', True, 'black', '#998484')
-score = font.render('Score is:', True, 'black', '#998484')
-replay= font.render('Press space to play' , True , 'black' , '#998484')
+    death = font.render('YOU DIED', True, 'black', '#998484')
+    score = font.render('Score is:', True, 'black', '#998484')
+    replay= font.render('Press space to play' , True , 'black' , '#998484')
 
-running = False 
+    running = False 
 
-while not running:
-
-
-   for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-          exit()
-
-   
-   pygame.draw.rect(window, '#998484', (0, 0, 400, 400))
-
-   window.blit(death,  (135,50) )
-   window.blit(score,  (10, 150))
-   window.blit(replay, (10,350))
- 
-
-   
-        #quit if they press space
-   keys = pygame.key.get_pressed()
-   if keys[pygame.K_SPACE]:
-       running = False
-
-   pygame.display.flip()
+    while not running:
 
 
-pygame.quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+
+    
+    pygame.draw.rect(window, '#998484', (0, 0, 400, 400))
+
+    window.blit(death,  (135,50) )
+    window.blit(score,  (10, 150))
+    window.blit(replay, (10,350))
+    
+
+    
+            #quit if they press space
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        running = False
+
+    pygame.display.flip()
+
+
+    pygame.quit()
