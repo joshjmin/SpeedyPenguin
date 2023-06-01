98a13569f915d81d30db6cff615685095bf221b3
Josh_Min <joshuamin2006@hotmail.com>
2023-06-01 12:30:31 -0400

    g

--- tests/opening_text.py
+++ tests/opening_text.py
@@ -5,18 +5,14 @@ app = App()
 
 def open_text():
 
-# Set up the window
-    pygame.init()
-    pygame.display.set_caption('My very first pygame GUI')
-    window = pygame.display.set_mode([300, 300])
-
 # Set up text module
     pygame.font.init()
-    font = pygame.font.SysFont('New times roman', 16)
+    font = pygame.font.SysFont('New times roman', 30)
 
 # Define text
-    text = font.render('W/UP;Jump and S/DOWN; slide', True, 'black', '#A5F2F3')
-    text_2 = font.render('Press space to play' , True , 'black' , '#A5F2F3')
+    text = font.render('PRESS W/UP to Jump', True, 'black', '#A5F2F3')
+    text_2 = font.render('PRESS S/DOWN to Slide', True, 'black', '#A5F2F3')
+    text_3 = font.render('PRESS SPACE to Play' , True , 'black' , '#A5F2F3')
 # Main loop
     running = True
     while running:
@@ -31,8 +27,9 @@ def open_text():
         pygame.draw.rect(window, '#A5F2F3', (0, 0, 300, 300))
 
     # Draw text
-        window.blit(text, (10, 100))
-        window.blit(text_2 , (10,150))
+        window.blit(text, (20, 60))
+        window.blit(text_2, (20, 100))
+        window.blit(text_3 , (20,200))
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE]:
