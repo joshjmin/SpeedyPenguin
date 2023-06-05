97b8ecf9e57930242b594efb911a3a35ba71cc2f
Julia <julia_chung@tdchristian.ca>
2023-06-05 12:32:15 -0400

    k

--- terms/__pycache__/close_page.py
+++ terms/__pycache__/close_page.py
@@ -1,28 +1,33 @@
 import pygame
 from gameplay import gameplay
+
+
 def death():
+
+    #Sets up the score function 
     score = gameplay()
+
+    #Creates window 
     pygame.init()
     pygame.display.set_caption('Speedy Penguin DEATH')
     window = pygame.display.set_mode([400, 400])
 
+    #Define text size and font type 
     pygame.font.init()
     font = pygame.font.SysFont('New times roman', 35)
 
+    #Draw text 
     death = font.render('YOU DIED', True, 'black', '#998484')
     score = font.render(f'Score is: {score}', True, 'black', '#998484')
-<<<<<<< HEAD
     replay= font.render('Press SPACE to Play' , True , 'black' , '#998484')
     quiting = font.render('Press Q to Quit' , True , 'black' , '#998484')
-=======
-    replay= font.render('Press space to play again' , True , 'black' , '#998484')
-    quiting = font.render('Press Q to quit' , True , 'black' , '#998484')
->>>>>>> cf719d286f180ee81d3cb8fdf0af220321e916f6
 
     running = False 
 
+    #Main Loop 
     while not running:
 
+        #Check if quit then quit 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
@@ -30,16 +35,18 @@ def death():
     
         pygame.draw.rect(window, '#998484', (0, 0, 400, 400))
 
+        #Place text on window using graph points 
         window.blit(death,  (135,50) )
         window.blit(score,  (10, 150))
         window.blit(replay, (10,350))
         window.blit(quiting , (10 , 250))
     
+        #Open dizzy penguin 
         dead = pygame.image.load('src/assets/dizzy_penguin.png')
         dead = pygame.transform.scale(dead , (150,130))
         window.blit(dead , (200 , 200))
     
-            #quit if they press space
+        #quit if they press space
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE]:
             running = True
