f87cca48c3df22610c2740c6626e43f82b422da3
Julia <julia_chung@tdchristian.ca>
2023-06-06 12:45:56 -0400

    k

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 from title_text import title_text
 from opening_text import open_text
 from close_page import death
+
+"""
+
+This code combines all of the other codes to create a 
+working game that has all 3 opening screans, the 
+game its self and the death screen.
+
+"""
+
 playing = True
+
 title_text()
 open_text()
+
 while playing:
     death()
 exit()
+
