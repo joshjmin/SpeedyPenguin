d25539f5be54efb576c4181a21821590b3f45674
Colin <colin.falconer03@gmail.com>
2023-06-08 13:06:17 -0400

    ,

--- tests/org.test.py
+++ tests/org.test.py
@@ -86,95 +86,140 @@ def gameplay() -> None:
         motion = 'standing'
         frame = 0
         active = False
-    #definitions
+    
     def flyer():
         app.flyerx -= 4 + speed - mode
         if app.flyerx < 150:
             app.flyery -= 4 + speed - mode
             app.flyerx -= 2 + speed - mode
+    
     def standerd():
         app.standerdx -= 3 + speed - mode
+    
     def fast():
         app.fastx -= 6 + speed - mode
+    
     def diver():
         if app.divery <= 325:
             app.divery += 3 + speed - mode
         app.diverx -= 2 + speed - mode
         app.uperx = app.diverx 
         app.upery = app.divery - 30
+    
     def middle():
         app.middlex -= 3 + speed - mode
         app.uperx = app.middlex 
         app.upery = app.middley - 30
-
-    #generate assets
-    if mode == 1:
-        penguin = pygame.image.load('src/assets/penguin_standing.png')    
-        penguin = pygame.transform.scale(penguin , (40,50))
-        penguin_jump = pygame.image.load('src/assets/flyingpenguin(pink).png')
-        penguin_jump = pygame.transform.scale(penguin_jump , (50 , 50))
-        penguin_slide = pygame.image.load('src/assets/penguin_sliding(pink).png')
-        penguin_slide = pygame.transform.scale(penguin_slide , (40,40))
-    elif mode == 0:
-        penguin = pygame.image.load('src/assets/penguin_standing(normal).png')    
-        penguin = pygame.transform.scale(penguin , (40,50))
-        penguin_jump = pygame.image.load('src/assets/flyingpenguin(normal).png')
-        penguin_jump = pygame.transform.scale(penguin_jump , (50 , 50))
-        penguin_slide = pygame.image.load('src/assets/penguin_sliding(normal).png')
-        penguin_slide = pygame.transform.scale(penguin_slide , (40,40))
-    else:
-        penguin = pygame.image.load('src/assets/penguin_standing(hard).png')    
-        penguin = pygame.transform.scale(penguin , (40,50))
-        penguin_jump = pygame.image.load('src/assets/flyingpenguin(hard).png')
-        penguin_jump = pygame.transform.scale(penguin_jump , (50 , 50))
-        penguin_slide = pygame.image.load('src/assets/penguin_sliding(hard).png')
-        penguin_slide = pygame.transform.scale(penguin_slide , (40,40))
     
-    ice = pygame.image.load('sawczak_demo/assets/iceberg.png')
-    ice = pygame.transform.scale(ice , (100,90 ))
-    ice2 = pygame.image.load('src/assets/isicle.png')    
-    ice2 = pygame.transform.scale(ice2 , (30,30))
-    ice2 = pygame.transform.rotate(ice2 , 270)
-    active = True
-    #main loop 
-    while active:
-    #ticks and frame data
-        clock.tick(200)
-    #collect fram data
-        frame += 1
-        if frame == 4:
-            frame = 1
-        #speed up
-        gtime += 1
-        speed += (gtime /  5000000)
-        #what level is the speed at
-        level = int((gtime // 1000) + 1)
+    def drawings():
+    #generate assets
+        if mode == 1:
+            penguin = pygame.image.load('src/assets/penguin_standing.png')    
+            penguin = pygame.transform.scale(penguin , (40,50))
+            penguin_jump = pygame.image.load('src/assets/flyingpenguin(pink).png')
+            penguin_jump = pygame.transform.scale(penguin_jump , (50 , 50))
+            penguin_slide = pygame.image.load('src/assets/penguin_sliding(pink).png')
+            penguin_slide = pygame.transform.scale(penguin_slide , (40,40))
+        elif mode == 0:
+            penguin = pygame.image.load('src/assets/penguin_standing(normal).png')    
+            penguin = pygame.transform.scale(penguin , (40,50))
+            penguin_jump = pygame.image.load('src/assets/flyingpenguin(normal).png')
+            penguin_jump = pygame.transform.scale(penguin_jump , (50 , 50))
+            penguin_slide = pygame.image.load('src/assets/penguin_sliding(normal).png')
+            penguin_slide = pygame.transform.scale(penguin_slide , (40,40))
+        else:
+            penguin = pygame.image.load('src/assets/penguin_standing(hard).png')    
+            penguin = pygame.transform.scale(penguin , (40,50))
+            penguin_jump = pygame.image.load('src/assets/flyingpenguin(hard).png')
+            penguin_jump = pygame.transform.scale(penguin_jump , (50 , 50))
+            penguin_slide = pygame.image.load('src/assets/penguin_sliding(hard).png')
+            penguin_slide = pygame.transform.scale(penguin_slide , (40,40))
+        
+        ice = pygame.image.load('sawczak_demo/assets/iceberg.png')
+        ice = pygame.transform.scale(ice , (100,90 ))
+        ice2 = pygame.image.load('src/assets/isicle.png')    
+        ice2 = pygame.transform.scale(ice2 , (30,30))
+        ice2 = pygame.transform.rotate(ice2 , 270)
+        pygame.draw.rect(window , '#000064' , (0,0,400,400))
+        pygame.draw.rect(window , '#00FFFF' , (0,350,400,400))
+        #draw the charecters
+        if motion == 'standing':
+            window.blit(penguin, (app.p_x - 30  , app.p_y - 30))
+        elif motion == 'jumping':
+            window.blit(penguin_jump , (app.p_x - 30 , app.p_y - 30))
+        else:
+            window.blit(penguin_slide , (app.p_x - 30 , app.p_y - 20))
 
-        #check if they quit
+        window.blit (ice, (app.standerdx - 45  , app.standerdy - 35))
+        window.blit(ice2 , (app.fastx - 20, app.fasty - 25))
+        window.blit(ice2 , (app.flyerx - 20, app.flyery - 25))
+        window.blit(ice2 , (app.diverx - 20 , app.divery - 25))
+        window.blit(ice2 , (app.diverx - 20, app.divery - 25))
+        window.blit(ice2 , (app.middlex - 20, app.middley - 25))
+        window.blit(ice2 , (app.middlex - 20, app.middley - 25))
+        window.blit(ice2 , (app.uperx - 20, app.upery - 25))
+
+        #jump meter
+        pygame.draw.rect(window , 'gray' , (0,0, 100 , 40))
+        if time <= 23:
+            pygame.draw.rect(window , 'red' , (5,5,5,30)) 
+        if time <= 22:
+            pygame.draw.rect(window, 'red' , (10,5,10,30))
+        if time <= 19:
+            pygame.draw.rect(window, 'red' , (15,5,15,30))
+        if time <= 17:
+            pygame.draw.rect(window, 'orange' , (20,5,20,30))
+        if time <= 14:
+            pygame.draw.rect(window, 'orange' , (25,5,25,30))
+        if time <= 12:
+            pygame.draw.rect(window, 'orange' , (30,5,30,30))
+        if time <= 10:
+            pygame.draw.rect(window, 'yellow' , (35,5,35,30))
+        if time <= 8:
+            pygame.draw.rect(window, 'yellow' , (40,5,40,30))
+        if time <= 6:
+            pygame.draw.rect(window, 'yellow' , (45,5,45,30))
+        if time <= 3:
+            pygame.draw.rect(window, 'green' , (50,5,40,30))
+        if time <= 0:
+            pygame.draw.rect(window, 'green' , (55 , 5 , 40 , 30))
+        #making a score board
+        font = pygame.font.SysFont('New times roman', 24)
+        app.text = font.render(f'LEVEL: {level}', True, '#0000FF', '#00FFFF')
+        window.blit(app.text , (300,375))
+        #score
+        font_2 = pygame.font.SysFont('New times roman', 24)
+        app.text_2 = font_2.render(f'score: {gtime}', True, '#0000FF', '#00FFFF')
+        window.blit(app.text_2 , (100,375))
+        print(falling_air)
+        pygame.display.flip()
+    
+    def quit():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
 
-        #reset the projectiles
+    def reset_proj():
         if app.standerdx < -20:
             app.standerdx = 450
             com = 1
-        if app.flyerx < -20:
+        elif app.flyerx < -20:
             app.flyerx = 450
             app.flyery = 350
             com = 1
-        if app.fastx < -20:
+        elif app.fastx < -20:
             app.fastx = 450
             com = 1
-        if app.diverx <= -20: 
+        elif app.diverx <= -20: 
             app.diverx = 450
             app.divery = 0
             com = 1
-        if app.middlex <= -20:
+        elif app.middlex <= -20:
             app.middlex = 450
             com = 1
-
-        #making the projictle move and projectile movement
+        else:
+            com = 1
+        
         if com == 1:
             com = 0
             #set restrictions based on level
@@ -186,7 +231,7 @@ def gameplay() -> None:
                 a = random.randint(0,4)
         #make standered iceberg move
         if a == 0:
-            standerd()
+            app.standerdx -= 3 + speed - mode
         #make fast move
         elif a == 2:
             app.fastx -= 5 + speed - mode
@@ -211,12 +256,15 @@ def gameplay() -> None:
             app.uperx = app.middlex 
             app.upery = app.middley - 30
 
+    def player_move():
         #calculate air time to incrase the fall speed
         if app.p_y < 350:
             air += 0.13
         if app.p_y == 350:
             air = 0
             falling_air = 0
+        if gtime <= 10:
+            time = 0 
 
         #track inputs
         #keys means if a key is pressed
@@ -267,6 +315,29 @@ def gameplay() -> None:
             if time > 0:
                 time -= 1
 
+
+    active = True
+    #main loop 
+    while active:
+    #ticks and frame data
+        clock.tick(200)
+    #collect fram data
+        time = 0
+        #speed up
+        gtime += 1
+        speed += (gtime /  5000000)
+        #what level is the speed at
+        level = int((gtime // 1000) + 1)
+
+        #check if they quit
+        quit()
+
+        #reset the projectiles
+        reset_proj()
+
+        #making the projictle move and projectile movement
+        player_move()
+
         #check for collisions
         if app.p_x - app.standerdx < 25 and app.p_x - app.standerdx > -25 and app.p_y - app.standerdy < 25 and app.p_y - app.standerdy > -25:
             reset()
@@ -286,64 +357,7 @@ def gameplay() -> None:
         if app.p_x - app.uperx < 25 and app.p_x - app.uperx > -25 and app.p_y - app.upery < 25 and app.p_y - app.upery > -25:
             reset()
             return gtime
-
-        #drawings
-        #TODO make animation for the walking of the main player
-        #TODO make a backround
-        pygame.draw.rect(window , '#000064' , (0,0,400,400))
-        pygame.draw.rect(window , '#00FFFF' , (0,350,400,400))
-        #draw the charecters
-        if motion == 'standing':
-            window.blit(penguin, (app.p_x - 30  , app.p_y - 30))
-        elif motion == 'jumping':
-            window.blit(penguin_jump , (app.p_x - 30 , app.p_y - 30))
-        else:
-            window.blit(penguin_slide , (app.p_x - 30 , app.p_y - 20))
-
-        window.blit (ice, (app.standerdx - 45  , app.standerdy - 35))
-        window.blit(ice2 , (app.fastx - 20, app.fasty - 25))
-        window.blit(ice2 , (app.flyerx - 20, app.flyery - 25))
-        window.blit(ice2 , (app.diverx - 20 , app.divery - 25))
-        window.blit(ice2 , (app.diverx - 20, app.divery - 25))
-        window.blit(ice2 , (app.middlex - 20, app.middley - 25))
-        window.blit(ice2 , (app.middlex - 20, app.middley - 25))
-        window.blit(ice2 , (app.uperx - 20, app.upery - 25))
-
-        
-        
-
-        #jump meter
-        pygame.draw.rect(window , 'gray' , (0,0, 100 , 40))
-        if time <= 23:
-            pygame.draw.rect(window , 'red' , (5,5,5,30)) 
-        if time <= 22:
-            pygame.draw.rect(window, 'red' , (10,5,10,30))
-        if time <= 19:
-            pygame.draw.rect(window, 'red' , (15,5,15,30))
-        if time <= 17:
-            pygame.draw.rect(window, 'orange' , (20,5,20,30))
-        if time <= 14:
-            pygame.draw.rect(window, 'orange' , (25,5,25,30))
-        if time <= 12:
-            pygame.draw.rect(window, 'orange' , (30,5,30,30))
-        if time <= 10:
-            pygame.draw.rect(window, 'yellow' , (35,5,35,30))
-        if time <= 8:
-            pygame.draw.rect(window, 'yellow' , (40,5,40,30))
-        if time <= 6:
-            pygame.draw.rect(window, 'yellow' , (45,5,45,30))
-        if time <= 3:
-            pygame.draw.rect(window, 'green' , (50,5,40,30))
-        if time <= 0:
-            pygame.draw.rect(window, 'green' , (55 , 5 , 40 , 30))
-        #making a score board
-        font = pygame.font.SysFont('New times roman', 24)
-        app.text = font.render(f'LEVEL: {level}', True, '#0000FF', '#00FFFF')
-        window.blit(app.text , (300,375))
-        #score
-        font_2 = pygame.font.SysFont('New times roman', 24)
-        app.text_2 = font_2.render(f'score: {gtime}', True, '#0000FF', '#00FFFF')
-        window.blit(app.text_2 , (100,375))
-        print(falling_air)
-        pygame.display.flip()
+        drawings()
     pygame.quit()
+
+gameplay()
